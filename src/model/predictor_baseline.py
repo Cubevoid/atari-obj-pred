8e85ac075e70e665a8155ca1362fa53a71a48595
Ben Edidin <benedidin@gmail.com>
2024-03-12 19:36:07 -0400

    Added predictor baseline model

---
+++ src/model/predictor_baseline.py
@@ -0,0 +1,20 @@
+from typing import Optional
+import torch
+from torch import nn
+import torch.nn.functional as F
+import wandb
+
+class PredictorBaseline(nn.Module):
+    def __init__(self, input_size: int = 128, time_steps: int = 5):
+        super().__init__()
+        self.time_steps = time_steps
+        self.fc1 = nn.Linear(input_size, input_size)
+        self.fc2 = nn.Linear(input_size, 2)
+
+    def forward(self, x):
+        predictions = []
+        for _ in range(self.time_steps):
+            predictions.append(F.relu(self.fc1(x)))
+        x = torch.stack(predictions, 1)
+        x = F.sigmoid(self.fc2(x))
+        return x
