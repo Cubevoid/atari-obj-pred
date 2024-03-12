37e17d3803ccc962e3ab0dd095041fa6c1f59c74
Jasper Gerigk <jasper.gerigk@mail.utoronto.ca>
2024-03-12 18:40:48 -0400

    get training on real data running

--- src/scripts/train_prediction.py
+++ src/scripts/train_prediction.py
@@ -13,25 +13,31 @@ from src.model.predictor import Predictor
 
 
 @hydra.main(version_base=None, config_path="../../configs/training", config_name="config")
-def train(config: DictConfig, batch_size: int = 4, t_steps: int = 1, num_obj: int = 4, name: str = 'debug') -> None:
+def train(config: DictConfig) -> None:
+    batch_size = config.batch_size
+    time_steps = config.time_steps
+    game = config.game
+    name = config.name
+    num_obj = config.num_objects
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
 
-    data_loader = DataLoader("SimpleTestData", num_obj)
+    data_loader = DataLoader(game, num_obj)
 
     feature_extract = FeatureExtractor(num_objects=num_obj).to(device)
-    predictor = Predictor(num_layers=1, time_steps=t_steps).to(device)
+    predictor = Predictor(num_layers=1, time_steps=time_steps).to(device)
 
-    wandb.init(project="oc-data-collection", entity="atari-obj-pred", name=name, config=typing.cast(Dict[Any, Any], OmegaConf.to_container(config)))
+    wandb.init(project="oc-data-training", entity="atari-obj-pred", name=name + game, config=typing.cast(Dict[Any, Any], OmegaConf.to_container(config)))
     wandb.log({"batch_size": batch_size})
-    wandb.watch(feature_extract, log="all", log_freq=1, idx=1)
-    wandb.watch(predictor, log="all", log_freq=1, idx=2)
+    wandb.watch(feature_extract, log="gradients", log_freq=100, idx=1)
+    wandb.watch(predictor, log="gradients", log_freq=100, idx=2)
 
     criterion = nn.MSELoss().to(device)
     optimizer = torch.optim.Adam(list(feature_extract.parameters()) + list(predictor.parameters()), lr=1e-3)
 
-    for _ in tqdm(range(100)):
-        images, bboxes, masks, _ = data_loader.sample(batch_size, t_steps)
+    for _ in tqdm(range(1000)):
+        images, bboxes, masks, _ = data_loader.sample(batch_size, time_steps)
         images, bboxes, masks = images.to(device), bboxes.to(device), masks.to(device)
         target = bboxes[:,:,:,:2]  # [B, T, O, 2]
 
@@ -39,9 +45,18 @@ def train(config: DictConfig, batch_size: int = 4, t_steps: int = 1, num_obj: in
         output: torch.Tensor = predictor(features)
         loss: torch.Tensor = criterion(output, target)
         loss.backward()
+        diff = torch.pow(output - target, 2)
+        diff_by_time = diff.mean(-1).mean(-1).mean(0)
         optimizer.step()
-        tqdm.write(f"{loss.item()=}, {output.mean().item()=}, {output.std().item()=}")
-        wandb.log({"loss": loss})
+        tqdm.write(f"loss={loss.item()}, output_mean={output.mean().item()}, std={output.std().item()}")
+        tqdm.write(f"target_mean={target.mean().item()} std={target.std().item()}")
+        error_dict = {"loss": loss,
+                    "error/x": diff[:,:,:,0].mean(),
+                    "error/y": diff[:,:,:,1].mean()
+        }
+        for t in range(time_steps):
+            error_dict[f"error/time_{t}"] = diff[:, t, :, :].mean()
+        wandb.log(error_dict)
         optimizer.zero_grad()
 
 
