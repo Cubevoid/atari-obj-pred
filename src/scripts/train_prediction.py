eb8cd0f31b83e15e66ce85a4b02cdeafc57211a6
Cubevoid <52008334+Cubevoid@users.noreply.github.com>
2024-03-15 11:21:43 -0400

    MLP predictor for curr pos, good performance

--- src/scripts/train_prediction.py
+++ src/scripts/train_prediction.py
@@ -13,6 +13,7 @@ import wandb
 from src.data_collection.data_loader import DataLoader
 from src.model.feat_extractor import FeatureExtractor
 from src.model.predictor import Predictor
+from src.model.mlp_predictor import MLPPredictor
 
 
 @hydra.main(version_base=None, config_path="../../configs/training", config_name="config")
@@ -22,6 +23,8 @@ def train(config: DictConfig) -> None:
     game = config.game
     name = config.name
     num_obj = config.num_objects
+    use_mlp = True if config.predictor == "mlp" else False
+    num_iterations = config.num_iterations
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
@@ -29,7 +32,11 @@ def train(config: DictConfig) -> None:
     data_loader = DataLoader(game, num_obj)
 
     feature_extract = FeatureExtractor(num_objects=num_obj).to(device)
-    predictor = Predictor(num_layers=1, time_steps=time_steps).to(device)
+    if use_mlp:
+        predictor = MLPPredictor().to(device)
+        time_steps = 1
+    else:
+        predictor = Predictor(num_layers=1, time_steps=time_steps).to(device)
 
     wandb.init(project="oc-data-training", entity="atari-obj-pred", name=name + game, config=typing.cast(Dict[Any, Any], OmegaConf.to_container(config)))
     wandb.log({"batch_size": batch_size})
@@ -39,7 +46,7 @@ def train(config: DictConfig) -> None:
     criterion = nn.MSELoss().to(device)
     optimizer = torch.optim.Adam(list(feature_extract.parameters()) + list(predictor.parameters()), lr=1e-3)
 
-    for i in tqdm(range(1000)):
+    for i in tqdm(range(num_iterations)):
         images, bboxes, masks, _ = data_loader.sample(batch_size, time_steps)
         images, bboxes, masks = images.to(device), bboxes.to(device), masks.to(device)
         target = bboxes[:,:,:,:2]  # [B, T, O, 2]
@@ -50,15 +57,9 @@ def train(config: DictConfig) -> None:
         loss.backward()
         diff = torch.pow(output - target, 2)
         optimizer.step()
-        tqdm.write(f"loss={loss.item()}, output_mean={output.mean().item()}, std={output.std().item()}")
-        tqdm.write(f"target_mean={target.mean().item()} std={target.std().item()}")
-        error_dict = {"loss": loss,
-                    "error/x": diff[:,:,:,0].mean(),
-                    "error/y": diff[:,:,:,1].mean()
-        }
-        for t in range(time_steps):
-            error_dict[f"error/time_{t}"] = diff[:, t, :, :].mean()
         if i % 50 == 0:
+            tqdm.write(f"loss={loss.item()}, output_mean={output.mean().item()}, std={output.std().item()}")
+            tqdm.write(f"target_mean={target.mean().item()} std={target.std().item()}")
             notzero = torch.nonzero(target)
             l1sum = 0
             total = 0
@@ -66,7 +67,14 @@ def train(config: DictConfig) -> None:
                 l1sum += abs(target[index[0]][index[1]][index[2]][index[3]]-output[index[0]][index[1]][index[2]][index[3]])
                 total += 1
             tqdm.write(f"l1 average loss = {l1sum/total}")
-            error_dict["l1average"] = (l1sum/total)
+            tqdm.write(f"Predicted: {output[:,:,0]}, Target: {target[:,:,0]}")
+        error_dict = {"loss": loss,
+                    "error/x": diff[:,:,:,0].mean(),
+                    "error/y": diff[:,:,:,1].mean()
+        }
+        for t in range(time_steps):
+            error_dict[f"error/time_{t}"] = diff[:, t, :, :].mean()
+        error_dict["l1average"] = (l1sum/total)
         wandb.log(error_dict)
         optimizer.zero_grad()
 
@@ -74,7 +82,8 @@ def train(config: DictConfig) -> None:
     unix_time = int(time.time())
     os.makedirs(to_absolute_path(f"./models/trained/{game}"), exist_ok=True)
     torch.save(feature_extract.state_dict(), to_absolute_path(f"./models/trained/{game}/{unix_time}_feat_extract.pth"))
-    torch.save(predictor.state_dict(), to_absolute_path(f"./models/trained/{game}/{unix_time}_predictor.pth"))
+    model_name = "mlp_predictor" if use_mlp else "transformer_predictor"
+    torch.save(predictor.state_dict(), to_absolute_path(f"./models/trained/{game}/{unix_time}_{model_name}.pth"))
 
 if __name__ == "__main__":
     train()  # pylint: disable=no-value-for-parameter
