2d419c65e36c2b10d6b06ee17c205c197bc14707
Ben Edidin <benedidin@gmail.com>
2024-03-30 14:57:36 -0400

    Test the model using the ground truth bounding boxes

--- src/scripts/train_prediction.py
+++ src/scripts/train_prediction.py
@@ -31,7 +31,8 @@ def train(cfg: DictConfig) -> None:
     predictor = (MLPPredictor() if use_mlp else Predictor(num_layers=1, time_steps=cfg.time_steps)).to(device)
     small_mlp = SmallMLP().to(device)
 
-    wandb.init(project="oc-data-training", entity="atari-obj-pred", name=cfg.name + cfg.game, config=typing.cast(Dict[Any, Any], OmegaConf.to_container(cfg)))
+    wandb.init(project="oc-data-training", entity="atari-obj-pred", name=cfg.name + cfg.game,
+               config=typing.cast(Dict[Any, Any], OmegaConf.to_container(cfg)))
     wandb.log({"batch_size": cfg.batch_size})
     wandb.watch(feature_extract, log="gradients", log_freq=100, idx=1)
     wandb.watch(predictor, log="gradients", log_freq=100, idx=2)
@@ -42,13 +43,21 @@ def train(cfg: DictConfig) -> None:
     for i in tqdm(range(cfg.num_iterations)):
         images, bboxes, masks, _ = data_loader.sample(cfg.batch_size, cfg.time_steps)
         images, bboxes, masks = images.to(device), bboxes.to(device), masks.to(device)
-        #target = bboxes[:, :, :, :2]  # [B, T, O, 2]
+        if cfg.ground_truth_masks:
+            bbox_ints = bboxes * 128
+            bbox_ints = bbox_ints.int()
+            masks = torch.zeros(masks.size())
+            for j in range(len(masks)):
+                for k in range(len(masks[j])):
+                    masks[j, k, int(bbox_ints[j][0][k][0]): int(bbox_ints[j][0][k][0] + bbox_ints[j][0][k][2]), int(bbox_ints[j][0][k][1]): int(bbox_ints[j][0][k][1] + bbox_ints[j][0][k][3])] = 1
+
+        target = bboxes[:, :, :, :2]  # [B, T, O, 2]
 
         # Run models
         features: torch.Tensor = feature_extract(images, masks)
         output: torch.Tensor = predictor(features)
-        target: torch.Tensor = mean(masks)
-        target = target.unsqueeze(1).repeat((1, 5, 1, 1))
+        # target: torch.Tensor = mean(masks)
+        # target = target.unsqueeze(1).repeat((1, 5, 1, 1))
         loss: torch.Tensor = criterion(output, target)
         loss.backward()
         optimizer.step()
@@ -61,8 +70,8 @@ def train(cfg: DictConfig) -> None:
             total = torch.sum(mask)
             tqdm.write(f"loss={loss.item()}, output_mean={output.mean().item()}, std={output.std().item()}")
             tqdm.write(f"target_mean={target.mean().item()} std={target.std().item()}")
-            tqdm.write(f"l1 average loss = {l1sum/total}")
-            #tqdm.write(f"Predicted: {output[:,:,0]}, Target: {target[:,:,0]}")
+            tqdm.write(f"l1 average loss = {l1sum / total}")
+            # tqdm.write(f"Predicted: {output[:,:,0]}, Target: {target[:,:,0]}")
             # tqdm.write(f"Std: {std} {std.shape}")
             # tqdm.write(f"Corr: {corr} {corr.shape}")
         error_dict = {"loss": loss, "error/x": diff[:, :, :, 0].mean(), "error/y": diff[:, :, :, 1].mean()}
@@ -76,7 +85,8 @@ def train(cfg: DictConfig) -> None:
     # save trained model to disk
     unix_time = int(time.time())
     os.makedirs(to_absolute_path(f"./models/trained/{cfg.game}"), exist_ok=True)
-    torch.save(feature_extract.state_dict(), to_absolute_path(f"./models/trained/{cfg.game}/{unix_time}_feat_extract.pth"))
+    torch.save(feature_extract.state_dict(),
+               to_absolute_path(f"./models/trained/{cfg.game}/{unix_time}_feat_extract.pth"))
     model_name = "mlp_predictor" if use_mlp else "transformer_predictor"
     torch.save(predictor.state_dict(), to_absolute_path(f"./models/trained/{cfg.game}/{unix_time}_{model_name}.pth"))
 
