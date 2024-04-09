207b282771ac8ba3b91a0169b9f34144f7a842a9
Cubevoid <52008334+Cubevoid@users.noreply.github.com>
2024-04-09 12:06:11 -0400

    Load and split data by frame instead of episode

--- src/data_collection/data_loader.py
+++ src/data_collection/data_loader.py
@@ -16,36 +16,43 @@ class DataLoader:
         self.load_data()
         self.history_len = 4
         self.num_obj = num_obj
-        self.num_train = int((train_pct * len(self.episode_data)) // self.history_len) * self.history_len
-        self.num_train = max(self.num_train, 1)
-        self.num_val = int((val_pct * len(self.episode_data)) // self.history_len) * self.history_len
-        self.num_test = len(self.episode_data) - self.num_train - self.num_val
+        self.num_train = int(((train_pct * len(self.frames))) // self.history_len) * self.history_len
+        self.num_val = int(((val_pct * len(self.frames))) // self.history_len) * self.history_len
+        self.num_test = len(self.frames) - self.num_train - self.num_val
 
     def load_data(self) -> None:
         """
         Load all the data from the disk
         """
         episodes_paths = [f for f in os.listdir(to_absolute_path(self.dataset_path)) if f.endswith(".npz")]
-        episode_counts = 0
-        episode_lengths = []
-        episode_data = {}
+        self.episode_count = 0
+        self.episode_lengths = []
+        episode_ids = set()
+        frames = []
+        object_types = []
+        object_bounding_boxes = []
+        detected_masks = []
+        actions = []
+        last_idx = []
         for episode in episodes_paths:
             data = np.load(to_absolute_path(self.dataset_path + episode))
-            episode_lengths.append(get_length_from_episode_name(episode))
+            self.episode_lengths.append(get_length_from_episode_name(episode))
             episode_id = get_id_from_episode_name(episode)
-            assert episode_id not in episode_data, f"Episode {episode_id} already exists in the dataset"
-            episode_data[episode_id] = (
-                data["episode_frames"],
-                data["episode_object_types"],
-                data["episode_object_bounding_boxes"],
-                data["episode_detected_masks"],
-                data["episode_actions"],
-                data["episode_last_idx"],
-            )
-            episode_counts += 1
-
-        self.episode_data = episode_data
-        self.episode_weights = np.array(episode_lengths) / sum(episode_lengths)
+            assert episode_id not in episode_ids, f"Episode {episode_id} already exists in the dataset"
+            episode_ids.add(episode_id)
+            frames.append(data["episode_frames"])
+            object_types.append(data["episode_object_types"])
+            object_bounding_boxes.append(data["episode_object_bounding_boxes"])
+            detected_masks.append(data["episode_detected_masks"])
+            actions.append(data["episode_actions"])
+            last_idx.append(data["episode_last_idx"])
+            self.episode_count += 1
+        self.frames = np.concatenate(frames, axis=0)
+        self.object_types = np.concatenate(object_types, axis=0)
+        self.object_bounding_boxes = np.concatenate(object_bounding_boxes, axis=0)
+        self.detected_masks = np.concatenate(detected_masks, axis=0)
+        self.actions = np.concatenate(actions, axis=0)
+        self.last_idx = np.concatenate(last_idx, axis=0)
 
     def sample(self, batch_size: int, time_steps: int, data_type: str = "train") -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         """
@@ -57,39 +64,43 @@ class DataLoader:
         Returns:
         Tuple containing stacked states [batch_size, time_steps=4, channels=3, H=128, W=128], Object bounding boxes [batch_size, t, num_objects, 4],
         Masks [batch_size, num_obj, H=128, W=128], Actions [batch_size, t]
+        - States are in the past
+        - Masks are in the present
+        - Bounding boxes, actions are in the future
         """
         if data_type == "train":
             start, end = 0, self.num_train
         elif data_type == "val":
             start, end = self.num_train, self.num_train + self.num_val
         elif data_type == "test":
-            start, end = self.num_train + self.num_val, len(self.episode_data)
-        weights = self.episode_weights[start:end]
-        weights = weights / weights.sum()  # renormalize probabilities to sum to 1
-        episodes = np.random.choice(np.arange(start, end), size=batch_size, p=weights)
+            start, end = self.num_train + self.num_val, len(self.frames)
+        frames = np.random.choice(np.arange(start, end), size=batch_size)
         states: List[npt.NDArray] = []
         object_bounding_boxes_list: List[npt.NDArray] = []
         masks: List[npt.NDArray] = []
         actions = []
-        for episode in episodes:
-            frames, _, object_bounding_boxes, detected_masks, episode_actions, last_idxs = self.episode_data[episode]
-            start = np.random.randint(0, len(frames) - self.history_len - time_steps)
-            base = start + self.history_len
-            states.append(frames[start:base])
-            obj_bbxs = object_bounding_boxes[base : base + time_steps]  # [T, O, 4]
-            objs = obj_bbxs[0].sum(-1) != 0  # [O]
-            orderd_bbxs = np.zeros_like(obj_bbxs)  # [T, O, 4] ordered by the initial object they are tracking
+        for idx in frames:
+            idx = min(idx, len(self.frames) - time_steps)  # Ensure we don't go out of bounds
+            idx = max(idx, self.history_len)
+            frame = self.frames[idx - self.history_len : idx]
+            object_bounding_boxes = self.object_bounding_boxes[idx : idx + time_steps]  # [T, O, 4] future bboxes
+            mask = self.detected_masks[idx]  # [O, H, W]
+            action = self.actions[idx : idx + time_steps]  # [T]
+            last_idxs = self.last_idx[idx : idx + time_steps]  # [T, O]
+            states.append(frame)
+            objs = object_bounding_boxes[0].sum(-1) != 0  # [O]
+            orderd_bbxs = np.zeros_like(object_bounding_boxes)  # [T, O, 4] ordered by the initial object they are tracking
             order = np.arange(objs.sum())  # [o]
             for t in range(time_steps):
-                orderd_bbxs[t, order] = obj_bbxs[t, objs]
-                order = last_idxs[base + t, order]
+                orderd_bbxs[t, order] = object_bounding_boxes[t, objs]
+                order = last_idxs[t, order]
             object_bounding_boxes_list.append(orderd_bbxs)
-            masks.append(detected_masks[base])
-            actions.append(episode_actions[base : base + time_steps])
+            masks.append(mask)
+            actions.append(action)
 
         states_tensor = torch.from_numpy(np.array(states))
         states_tensor = states_tensor / 255
-        states_tensor = states_tensor.permute(0, 4, 1, 2, 3)
+        states_tensor = states_tensor.permute(0, 1, 4, 2, 3)  # [B, T, C, H, W]
         object_bounding_boxes_tensor = torch.from_numpy(np.array(object_bounding_boxes_list))
         object_bounding_boxes_tensor = object_bounding_boxes_tensor.float()
         w = states_tensor.shape[-2]
